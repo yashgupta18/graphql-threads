268603e4d9cc7672bd49fd8cb9e493555335b068
yashgupta18 <18guptayash@gmail.com>
2024-11-04 18:18:31 +0530

    authorization

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { expressMiddleware } from '@apollo/server/express4';
 import createApolloGraphqlServer from './graphql'
+import UserService from './services/user';
 
 async function init() {
   const app = express()
@@ -13,7 +14,22 @@ async function init() {
 
   const gqlServer = await createApolloGraphqlServer()
   // Apply the ApolloServer instance to the Express app
-  app.use('/graphql', expressMiddleware(gqlServer))
+  app.use(
+    "/graphql",
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: async ({ req }) => {
+        // @ts-ignore
+        const token = req.headers["token"];
+
+        try {
+          const user = UserService.decodeJWTToken(token as string);
+          return { user };
+        } catch (error) {
+          return {};
+        }
+      },
+    })
+  );
 
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
